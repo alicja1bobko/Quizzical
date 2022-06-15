9fa75b44fcbc33feb284e8089e3aea8360533c12
AlicjaBobko <alicja_bobko@o2.pl>
2022-06-15 15:56:53 +0200

    deploy changes

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react'
 import './App.css';
 import yellowBlob from './images/yellow-blob.png';
 import blueBlob from './images/blue-blob.png';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { HashRouter , Route, Routes } from 'react-router-dom';
 import Menu from './Pages/Menu/Menu';
 import Quiz from './Pages/Quiz/Quiz';
 import { QuestionsProvider } from './components/QuestionsContext';
@@ -13,12 +13,12 @@ function App() {
     <QuestionsProvider>
     <main>
       <img id="yellow-blob" src={yellowBlob} alt="yellow blob" />
-        <BrowserRouter basename="https://alicja1bobko.github.io/Quizzical">
+        <HashRouter  basename="https://alicja1bobko.github.io/Quizzical">
            <Routes>
             <Route path="/" element={<Menu />} />
             <Route path="/quiz" element={<Quiz/>} />
            </Routes>
-        </BrowserRouter>
+        </HashRouter >
       <img id="blue-blob" src={blueBlob} alt="blue blob" />
       </main>
       </QuestionsProvider>
