cf458c2c6065a99daada0915ec79b5d770e56e14
AlicjaBobko <alicja_bobko@o2.pl>
2022-06-15 15:34:06 +0200

    deploy changes

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
     <QuestionsProvider>
     <main>
       <img id="yellow-blob" src={yellowBlob} alt="yellow blob" />
-        <BrowserRouter>
+        <BrowserRouter basename="https://alicja1bobko.github.io/Quizzical">
            <Routes>
             <Route path="/" element={<Menu />} />
             <Route path="/quiz" element={<Quiz/>} />
